57dbb5de68ec7da737df2dab9bed26ced6b609df
Juanjo Garcia <juanjo@garciaamaya.com>
2021-04-19 21:52:30 +0200

    Minor fixes, I'm learning

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const version = "v0.1"
-const hostname = '127.0.0.1';
+const version = "0.1.1"
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world, this is my ${version} try to js');
+    res.end(`Hello world, this is my ${version} try to js`);
 });
 
 server.listen(port, hostname, () => {
